d83423181e225adc55da7be13f2d332a32334d10
anlastochkin <anton_lastochkin@mail.ru>
2023-01-06 18:42:26 +0300

    add skif win console conversion

--- src/main.cpp
+++ src/main.cpp
@@ -32,25 +32,88 @@
 // #include <unistd.h>
 #include <locale>
 #include <codecvt>
-// #include <Windows.h>
+
+#ifdef WIN32
+#include <Windows.h>
+std::string utf8_to_cp1251(std::string const& utf8)
+{
+    if (!utf8.empty())
+    {
+        int wchlen = MultiByteToWideChar(CP_UTF8, 0, utf8.c_str(), utf8.size(), NULL, 0);
+        if (wchlen > 0 && wchlen != 0xFFFD)
+        {
+            std::vector<wchar_t> wbuf(wchlen);
+            int result_u = MultiByteToWideChar(CP_UTF8, 0, utf8.c_str(), utf8.size(), &wbuf[0], wchlen);
+            if (!result_u) {
+                throw std::runtime_error("Skif utf8_to_cp1251 cannot convert MultiByteToWideChar!");
+            }
+            std::vector<char> buf(wchlen);
+            int result_c = WideCharToMultiByte(1251, 0, &wbuf[0], wchlen, &buf[0], wchlen, 0, 0);
+            if (!result_c) {
+                throw std::runtime_error("Skif utf8_to_cp1251 cannot convert WideCharToMultiByte!");
+            }
+
+            return std::string(&buf[0], wchlen);
+        }
+    }
+    return std::string();
+}
+
+std::string cp1251_to_utf8(const std::string& cp1251) {
+    std::string res;
+    int result_u, result_c;
+    enum { CP1251 = 1251 };
+    result_u = MultiByteToWideChar(CP1251, 0, cp1251.c_str(), -1, 0, 0);
+    if (!result_u) {
+        throw std::runtime_error("Skif cp1251_to_utf8 cannot convert MultiByteToWideChar!");
+    }
+    wchar_t* ures = new wchar_t[result_u];
+    if (!MultiByteToWideChar(CP1251, 0, cp1251.c_str(), -1, ures, result_u)) {
+        delete[] ures;
+        throw std::runtime_error("Skif cp1251_to_utf8 cannot convert MultiByteToWideChar 2!");
+    }
+    result_c = WideCharToMultiByte(CP_UTF8, 0, ures, -1, 0, 0, 0, 0);
+    if (!result_c) {
+        delete[] ures;
+        throw std::runtime_error("Skif cp1251_to_utf8 cannot convert WideCharToMultiByte!");
+    }
+    char* cres = new char[result_c];
+    if (!WideCharToMultiByte(CP_UTF8, 0, ures, -1, cres, result_c, 0, 0)) {
+        delete[] cres;
+        throw std::runtime_error("Skif cp1251_to_utf8 cannot convert WideCharToMultiByte 2!");
+    }
+    delete[] ures;
+    res.append(cres);
+    delete[] cres;
+    return res;
+}
+
+#endif
+
+void print(const std::string& text)
+{
+#ifdef WIN32
+    std::cout << utf8_to_cp1251(text);
+#else
+    std::cout << text;
+#endif
+}
 
 int main() 
 {
-  
-  // setlocale(LC_ALL, "Russian");
-  // SetConsoleCP(1251);
-  // SetConsoleOutputCP(1251);
-  // system("chcp 1251");
-
+#ifdef WIN32
+    setlocale(LC_ALL, "RUS");
+    system("chcp 1251");
+#endif
   std::vector<jinx::WordQuest> Questions;  
   std::string fileName;
   int num = 0;
   
   std::system("clear");
-  std::cout << "---------------------------------------------------------------\n\n";
-  std::cout << "                  >>> Игра Угадай-ка! <<<\n\n";
-  std::cout << "---------------------------------------------------------------\n\n";
-  std::cout << "> Введите название файла с вопросами (def: quest.txt)\n: ";
+  print("---------------------------------------------------------------\n\n");
+  print("                  >>> Игра Угадай-ка! <<<\n\n");
+  print("---------------------------------------------------------------\n\n");
+  print("> Введите название файла с вопросами (def: quest.txt)\n: ");
   std::cin >> fileName;
   
   num = jinx::get_quests_amount(fileName);
